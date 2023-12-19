19ccdbe08328471f796cd88f21f5a17bc0051fca
Sean Boult <sboult@amazon.com>
2023-12-19 21:16:24 +0000

    Top level await and more TS

--- server.ts
+++ server.ts
@@ -1,38 +1,62 @@
-import http, { Server } from "node:http"
+import http, { Server } from "node:http";
+
+/**
+ * Represents a handle to a promise that isn't strictly associated with any
+ * async operation and rather depends on the exposed `resolve()` and `reject()`
+ * methods which can be used to settle it.
+ */
+export default class Deferred<T> {
+  promise: Promise<T>;
+  resolve: (value: T | PromiseLike<T>) => void;
+  reject: (reason: Error) => void;
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+}
+
+interface ServerStartResult {
+  server: Server | null;
+}
 
 function createServer() {
-  const server = http.createServer()
+  const server = http.createServer();
+  const deferredStartServerResult = new Deferred<ServerStartResult>();
 
   server.on("error", (error) => {
-    console.log("error", error)
+    console.log("error", error);
+    deferredStartServerResult.resolve({
+      server: null,
+    });
   });
 
-  server.listen(7000, '0.0.0.0', () => {
-    console.log("Staring server completed!")
+  server.listen(7000, "0.0.0.0", () => {
+    console.log("✅ Staring server completed!");
+    deferredStartServerResult.resolve({
+      server,
+    });
   });
 
-  return server;
+  return deferredStartServerResult.promise;
 }
 
 const closeServer = async (server: Server): Promise<void> =>
   new Promise((resolve, reject) => {
-    server.on("close", (err: unknown) => {
-      if (err) {
-        return reject(err);
-      }
+    server.on("close", () => {
+      console.log("🛑 Stopping server completed!");
       resolve();
     });
 
     server.close();
   });
 
+for (let i = 0; i < 4; i++) {
+  const server = await createServer();
 
-async function main() {
-
-  for (let i = 0; i < 10; i++) {
-    const server1 = createServer();
-    await closeServer(server1);
+  if (server.server) {
+    await closeServer(server.server);
   }
 }
-
-main();
